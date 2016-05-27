0c078352b203b32c8aa75d0edee8222a20a38eb5
Joseph Hughes <joe.hughes@ubnt.com>
2016-05-27 12:57:04 -0600

    initial push

---
+++ test/helper.rb
@@ -0,0 +1,51 @@
+require "simplecov"
+SimpleCov.start
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
+require 'rubygems'
+require 'bundler'
+require 'docker'
+require 'timecop'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+require 'test/unit'
+require 'pry'
+
+
+require "simplecov"
+require "codeclimate-test-reporter"
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'fluent/test'
+require 'webmock/test_unit'
+WebMock.disable_net_connect!(:allow => "codeclimate.com")
+
+if ENV.has_key?('VERBOSE')
+  $log = Fluent::Log.new(Fluent::Test::DummyLogDevice.new, Fluent::Log::LEVEL_TRACE)
+else
+  nulllogger = Object.new
+  nulllogger.instance_eval {|obj|
+    def method_missing(method, *args)
+      # pass
+    end
+  }
+  $log = nulllogger
+end
+Docker.url = 'tcp://example.com:5422'
+require 'fluent/plugin/filter_ecs_filter'
+
+class Test::Unit::TestCase
+end
---
+++ test/plugin/test_mesosphere.rb
@@ -0,0 +1,128 @@
+require 'helper'
+
+class ECSFilterTest < Test::Unit::TestCase
+  def setup
+    unless defined?(Fluent::Filter)
+      omit('Fluent::Filter is not defined. Use fluentd 0.12 or later.')
+    end
+
+    Fluent::Test.setup
+  end
+
+  # config_param :ratio, :float
+  # config_param :key_names, :string, :default => nil
+  # config_param :key_pattern, :string, :default => nil
+  # config_param :floor, :bool, :default => false
+  # config_param :remove_prefix, :string, :default => nil
+  # config_param :add_prefix, :string, :default => nil
+
+  CONFIG = %[]
+  CONFIG2 = %[
+    cache_size 2000
+    cache_ttl 300
+  ]
+  CONFIG3 = %[
+    get_container_id_tag true
+    container_id_attr container_id
+  ]
+
+  def create_driver(conf = CONFIG, tag = 'test')
+    Fluent::Test::FilterTestDriver.new(Fluent::ECSFilter, tag).configure(conf)
+  end
+
+  def setup_docker_stub(file, docker_api_url)
+    stub_request(:get, docker_api_url)
+      .to_return(status: 200, body: file, headers: {})
+  end
+
+  def setup_ecs_container(container_id, file_name)
+    docker_api_url = "http://tcp//example.com:5422/v1.16/containers/#{container_id}/json"
+    file = File.open("test/containers/#{file_name}.json", 'rb')
+    setup_docker_stub(file, docker_api_url)
+  end
+
+
+  def test_ecs_filter
+    setup_ecs_container('foobar123', 'ecs')
+
+    d1 = create_driver(CONFIG, 'docker.foobar123')
+    d1.run do
+      d1.filter('log' => 'Hello World 1')
+    end
+    filtered = d1.filtered_as_array
+
+    task_id = 'fdb86b4f-b919-4a65-89eb-4b3761eb8952'
+
+    log_entry = filtered[0][2]
+
+    assert_equal 'unifi-video', log_entry['task_family']
+    assert_equal '9', log_entry['task_version']
+    assert_equal task_id, log_entry['task_id']
+  end
+
+  def test_container_cache
+    setup_ecs_container('foobar123', 'ecs')
+
+    d1 = create_driver(CONFIG, 'docker.foobar123')
+    d1.run do
+      1000.times do
+        d1.filter('log' => 'Hello World 4')
+      end
+    end
+    docker_api_url = 'http://tcp//example.com:5422/v1.16/containers/foobar123/json'
+
+    assert_equal 1000, d1.filtered_as_array.length
+    assert_requested(:get, docker_api_url, times: 2)
+  end
+
+  def test_container_cache_expiration
+    setup_ecs_container('foobar123', 'ecs')
+
+    d1 = create_driver(CONFIG2, 'docker.foobar123')
+    d1.run do
+      d1.filter('log' => 'Hello World 4')
+    end
+
+    Timecop.travel(Time.now + 10 * 60)
+
+    d1.run do
+      d1.filter('log' => 'Hello World 4')
+    end
+
+    Timecop.return
+
+    docker_api_url = 'http://tcp//example.com:5422/v1.16/containers/foobar123/json'
+
+    assert_requested(:get, docker_api_url, times: 4)
+  end
+
+  def test_merge_json
+    setup_ecs_container('foobar123', 'ecs')
+
+    d1 = create_driver(CONFIG, 'docker.foobar123')
+    d1.run do
+      d1.filter('log' => '{"test_key":"Hello World"}')
+    end
+    filtered = d1.filtered_as_array
+    log_entry = filtered[0][2]
+
+    assert_equal 'Hello World', log_entry['test_key']
+  end
+
+  def test_bad_merge_json
+    setup_ecs_container('foobar123', 'ecs')
+    bad_json1 = '{"test_key":"Hello World"'
+    bad_json2 = '{"test_key":"Hello World", "badnews"}'
+
+    d1 = create_driver(CONFIG, 'docker.foobar123')
+    d1.run do
+      d1.filter('log' => bad_json1)
+      d1.filter('log' => bad_json2)
+    end
+    filtered = d1.filtered_as_array
+
+    assert_equal bad_json1, filtered[0][2]['log']
+    assert_equal bad_json2, filtered[1][2]['log']
+  end
+
+end
